85a4d22875a63f9e77919f8be1cba85b9ba0bc7b
Rob Stemp <robs@iaanyware.net>
2016-04-13 16:35:02 +1000

    Changed mongo async methods to use sync methods

--- NLog.MongoDB20/MongoRepository.cs
+++ NLog.MongoDB20/MongoRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Threading.Tasks;
@@ -6,84 +7,74 @@ namespace NLog.MongoDB20
 {
     using System.Collections.Generic;
 
-	internal sealed class MongoRepository : IRepository
-	{
-		private MongoClient _client;
-		private readonly string _database;
+    internal sealed class MongoRepository : IRepository
+    {
+        private IMongoClient _client;
+        private readonly string _database;
 
-		private static readonly Dictionary<string, bool> _collectionCache = new Dictionary<string, bool>();
+        private static readonly ConcurrentDictionary<string, bool> _collectionCache = new ConcurrentDictionary<string, bool>();
 
         public MongoRepository(MongoClientSettings settings, string databaseName)
-	    {
+        {
             _database = databaseName;
             _client = new MongoClient(settings);
-;
         }
 
         public void CheckCollection(string collectionName, long collectionSize, long? collectionMaxItems, bool createIdField)
         {
             var db = _client.GetDatabase(_database);
+            if (_collectionCache.ContainsKey(collectionName)) return;
 
-            lock (_collectionCache)
+            //Not really happy with this implementation in order to determine if a collection exists.
+            bool collectionExists = false;
+
+            using (var cursor = db.ListCollections())
             {
-                if (_collectionCache.ContainsKey(collectionName)) return;
-
-                //Not really happy with this implementation in order to determine if a collection exists.
-                bool collectionExists = false;
-
-                using (var cursor = db.ListCollectionsAsync().Result)
+                var collections = cursor.ToList();
+                foreach (var collection in collections)
                 {
-                    var collections = cursor.ToListAsync();
-                    foreach (var collection in collections.Result)
+                    if (collection["name"] == collectionName)
                     {
-                        if (collection["name"] == collectionName)
-                        {
-                            collectionExists = true;
-                        }       
+                        collectionExists = true;
                     }
                 }
-
-
-                if (!collectionExists)
-                {
-                    var collectionOptions = new CreateCollectionOptions()
-                    {
-                        Capped = true,
-                        MaxSize = collectionSize,
-                        AutoIndexId = createIdField
-                    };
-
-                    if (collectionMaxItems.HasValue)
-						collectionOptions.MaxDocuments =collectionMaxItems.Value;
-
-                    db.CreateCollectionAsync(collectionName, collectionOptions);
-
-
-                }
-
-                _collectionCache.Add(collectionName, createIdField);
             }
 
+            if (!collectionExists)
+            {
+                var collectionOptions = new CreateCollectionOptions()
+                {
+                    Capped = true,
+                    MaxSize = collectionSize,
+                    AutoIndexId = createIdField
+                };
+
+                if (collectionMaxItems.HasValue)
+                    collectionOptions.MaxDocuments = collectionMaxItems.Value;
+
+                db.CreateCollection(collectionName, collectionOptions);
+            }
+            _collectionCache.TryAdd(collectionName, createIdField);
         }
 
-	    public async void Insert(string collectionName, BsonDocument item)
-		{
-			var db = _client.GetDatabase(_database);
+        public void Insert(string collectionName, BsonDocument item)
+        {
+            var db = _client.GetDatabase(_database);
 
-		    IMongoCollection<BsonDocument> collection;
+            IMongoCollection<BsonDocument> collection;
 
-			// if we shouldn't save ids
-		    if (_collectionCache.ContainsKey(collectionName) && !_collectionCache[collectionName])
-			    collection = db.GetCollection<BsonDocument>(collectionName, new MongoCollectionSettings {AssignIdOnInsert = false});
-			else
-				collection = db.GetCollection<BsonDocument>(collectionName);
+            // if we shouldn't save ids
+            if (_collectionCache.ContainsKey(collectionName) && !_collectionCache[collectionName])
+                collection = db.GetCollection<BsonDocument>(collectionName, new MongoCollectionSettings { AssignIdOnInsert = false });
+            else
+                collection = db.GetCollection<BsonDocument>(collectionName);
 
-            await collection.InsertOneAsync(item);
+            collection.InsertOne(item);
         }
 
-		public void Dispose()
-		{
-			_client = null;
-		}
-	}
+        public void Dispose()
+        {
+            _client = null;
+        }
+    }
 }
